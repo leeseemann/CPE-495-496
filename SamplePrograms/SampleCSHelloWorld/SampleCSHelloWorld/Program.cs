0a0157489ad2c23499fb3ae5774ddcb0bdb3d918
Elaine Boyd <elaine.fagerman@gmail.com>
2015-11-02 07:44:44 -0600

    Sample CS Hello World
    Generic example of C# code

---
+++ SamplePrograms/SampleCSHelloWorld/SampleCSHelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿//A hello world sample c# program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello WORLD!!!!!!");
+
+            //keep console window open in debug mode.
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
+        }
+    }
+}
