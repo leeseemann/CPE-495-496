5c5830e5f789d51c4d97a0fa07ebae35828f2fc2
leeseemann <ls0013@uah.edu>
2016-01-25 13:32:32 -0600

    Retrieve Kinect Frame
    1) added code to retrieve a frame from the kinect

--- Code/profile.cpp
+++ Code/profile.cpp
@@ -8,7 +8,7 @@ Modification History
 Developer		Date			Comments
 --------------------------------------------------------------------------------
 Lee				12/22/15		file created, added initialize() function
-
+Lee				1/25/16			added initialize_Kinect() and getKinectFrame() functions
 
 --------------------------------------------------------------------------------
 */
@@ -22,8 +22,65 @@ profile::profile()
 
 void profile::initialize()
 {
-	cout << "Initializing Profile Verification Software" << endl;
+	cout << "Initializing Profile Verification Software" << endl; \
+	init_Kinect = initialize_Kinect();
+
 	return;
+}
+
+bool profile::initialize_Kinect()
+{
+	int numSensors;
+	if (NuiGetSensorCount(&numSensors) < 0 || numSensors < 1) // determine if a Kinect is attached and running
+	{
+		cout << "ERROR: No Kinect Found" << endl;
+		return false;
+	}
+	if (NuiCreateSensorById(0, &sensor) < 0)
+	{
+		cout << "ERROR: Kinect Sensor Uninitialized" << endl;
+		return false;
+	}
+
+	sensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_DEPTH | NUI_INITIALIZE_FLAG_USES_COLOR); // initialize the Kinect and specify depth/color and flags
+	sensor->NuiImageStreamOpen(
+		NUI_IMAGE_TYPE_COLOR,          // depth or rgb camera
+		NUI_IMAGE_RESOLUTION_640x480,  // image resolution
+		0,							   // image stream flags
+		2,							   // number of frames to buffer
+		NULL,						   // event handler
+		&rgbStream);
+
+	return sensor;
+}
+
+void profile::getKinectFrame(GLubyte* destination)
+{
+	NUI_IMAGE_FRAME imageFrame;
+	NUI_LOCKED_RECT lockedRect;
+
+	if (sensor->NuiImageStreamGetNextFrame(rgbStream, 0, &imageFrame) < 0) // retrieve a frame from Kinect
+	{
+		cout << "ERROR: Failed to Retrieve Kinect Frame" << endl;
+		return;
+	}
+
+	texture = imageFrame.pFrameTexture;
+	texture->LockRect(0, &lockedRect, NULL, 0); // lock the frame
+
+	if (lockedRect.Pitch != 0) // check to see if there is data in the frame
+	{
+		current = (const unsigned char*)lockedRect.pBits;
+		dataEnd = current + (width*height) * 4;
+
+		while (current < dataEnd) // iterate through the frame
+		{
+			*destination++ = *current++;
+		}
+	}
+
+	texture->UnlockRect(0); // unlock the frame
+	sensor->NuiImageStreamReleaseFrame(rgbStream, &imageFrame); // release the frame
 }
 
 profile::~profile()
--- Code/profile.h
+++ Code/profile.h
@@ -9,6 +9,7 @@ Developer		Date			Comments
 --------------------------------------------------------------------------------
 Lee				12/22/15		profile class created, added initialize() function
 Lee				1/16/16			added header files for Kinect SDK
+Lee				1/25/16			added Kinect and OpenGL variables/functions for Kinect interaction
 
 
 --------------------------------------------------------------------------------
@@ -21,14 +22,39 @@ Lee				1/16/16			added header files for Kinect SDK
 #include <iomanip>
 #include <stdlib.h>
 #include <Windows.h>
+#include <Ole2.h>
 #include <NuiApi.h>
+#include <NuiImageCamera.h>
+#include <NuiSensor.h>
+#include <gl\GL.h>
+#include <gl\GLU.h>
+
+// define the size of the Kinect frame
+#define width 640
+#define height 480
 
 class profile
 {
 public:
 	profile(); // class constructor
 	void initialize();
+	bool initialize_Kinect();	// initialize the Kinect
+	void getKinectFrame(GLubyte* destination);  // retrieve a frame from the Kinect
 	~profile(); // class destructor 
+
+	//Kinect variables
+	HANDLE rgbStream;  // identifier of the Kinect RGB camera
+	INuiSensor* sensor; // Kinect sensor
+	bool init_Kinect = false;  // was Kinect successfully initialized
+	INuiFrameTexture* texture;
+	const unsigned char* current; // used to iterate through Kinect frame
+	const unsigned char* dataEnd; // used to iterate through Kinect frame
+
+	// OpenGL Variables
+	GLuint textureID;	// ID of the texture to contain RGB data from Kinect
+	GLubyte data[width * height * 4];  // array containing the texture data
+
+	
 };
 
 #endif
