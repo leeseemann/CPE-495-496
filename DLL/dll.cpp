5d8e54edb3e565d732a9b58ea4445483f9abc877
Lee Seemann <ls0013@uah.edu>
2016-02-21 13:15:55 -0600

    Cleanup
    cleanup

--- DLL/dll.cpp
+++ DLL/dll.cpp
@@ -15,20 +15,20 @@ Lee				1/31/16			added the parameter depth_data
 --------------------------------------------------------------------------------
 */
 
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\driver.h"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\driver.cpp"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\profile.h"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\profile.cpp"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\length.h"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\length.cpp"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\color.h"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\color.cpp"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\quantity.h"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\quantity.cpp"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\database.h"
-#include "A:\School\Spring 2016\CPE 496\Code\CPE_Senior_Design\Code\database.cpp"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\driver.h"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\driver.cpp"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\profile.h"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\profile.cpp"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\length.h"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\length.cpp"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\color.h"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\color.cpp"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\quantity.h"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\quantity.cpp"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\database.h"
+#include "C:\Users\Lee Seemann\Documents\GitHub\CPE_Senior_Design\Code\database.cpp"
 
-extern "C" __declspec(dllexport) void Steelcase_Answer_Verification(short depth_data)
+extern "C" __declspec(dllexport) void Steelcase_Answer_Verification(short depth_data[])
 {
 	driver project_driver;
 	project_driver.startup(depth_data);
